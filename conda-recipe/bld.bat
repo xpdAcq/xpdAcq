2acfc8d96efb6a18286f2e2e2f62741bb16dd0c9
chiahaoliu <chbrahmsliu@gmail.com>
2016-01-20 14:24:15 -0500

    fixed conda recipe
    works fine now

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -1,3 +1,2 @@
 "%PYTHON%" setup.py install
-"%PYTHON%" setup.py --version > __conda_version__.txt
 if errorlevel 1 exit 1
--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,3 +1,1 @@
 $PYTHON setup.py install
-
-$PYTHON setup.py --version > __conda_version__.txt 
