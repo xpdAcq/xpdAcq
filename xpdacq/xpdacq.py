62ff415adf18e8823e7adc51f305cf438b6abfb9
chiahaoliu <chbrahmsliu@gmail.com>
2016-01-21 12:39:44 -0500

    Before lunch, add RunEngine

--- xpdacq/xpdacq.py
+++ xpdacq/xpdacq.py
@@ -1,1 +1,29 @@
+## testing section ##
 from bluesky.scans import Count # fake object but exact syntax
+#####################
+
+
+## main module ##
+def _bluesky_global_state():
+    '''Import and return the global state from bluesky.'''
+
+    from bluesky.standard_config import gs
+    return gs
+
+def _bluesky_metadata_store():
+    '''Return the dictionary of bluesky global metadata.'''
+
+    gs = _bluesky_global_state()
+    return gs.RE.md
+
+def _bluesky_RE():
+    import bluesky
+    from bluesky.run_engine import RunEngine
+    from bluesky.register_mds import register_mds
+    #from bluesky.run_engine import DocumentNames
+    RE = RunEngine()
+    register_mds(RE)
+    return RE
+
+RE = _bluesky_RE()
+
