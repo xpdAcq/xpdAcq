51dab3e9a6628d3b84865081df1757c2edb2113c
chiahaoliu <chbrahmsliu@gmail.com>
2016-01-20 19:47:43 -0500

    add zip_safe = Fale
    avoid glitch in ipython

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'xpdacq',
-    version = '1.0.0',
+    version = '1.0.1',
     packages = ['xpdacq'],
     description = 'acquisition module',
+    zip_safe = False,
     url = 'http:/github.com/chiahaoliu/xpdacq'
     )
