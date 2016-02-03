f5a93e0f9ec08dfa2f47ce7921c0b7818553a51a
Timothy Liu <chbrahmsliu@gmail.com>
2016-02-02 22:27:53 -0500

    first acquisition func

--- xpdacq/control.py
+++ xpdacq/control.py
@@ -13,17 +13,65 @@
 #
 ##############################################################################
 
+def _get_obj(name):
+    ipshel = get_ipython()
+    return ipshell.user_ns[name]
+
 
 def _open_shutter():
     ''' open the shutter that is currently working. Maintain at every beamtime
     ''' 
+    import time
+    
+    # Shutter this time : shctl1
+    sh_name = 'shctl1'
+    sh = _get_obj(sh_name)
+    
     print('Open shutter')
-    # FIXME - verify it!
+    #shutter status
+    if sh.get() == 1:
+        pass
+    else:
+        print('shutter value before open_pv.put(1): %s' % sh.get())
+        # open shutter
+        shutter_try = 0
+        shutter_tries = 5
+        while sh.get() == 0 and shutter_try < shutter_tries:
+            sh.put(1)
+            time.sleep(2.)   
+            print('shutter value after open_pv.put(1): %s' % sh.get())
+            shutter_try += 1
+        if sh.get() == 0:
+            print('shutter failed to open after %i tries. Please check before continuing' % shutter_tries)
+            return
+
     print('Shutter opened')
 
 def _close_shutter():
     ''' close the shutter that is currently working. Maintain at every beamtime
     '''
+    import time
+    
+    # Shutter this time : shctl1
+    sh_name = 'shctl1'
+    sh = _get_obj(sh_name)
+    
     print('Close shutter')
-    # FIXME - verify it!
+    #shutter status
+    if sh.get() == 0:
+        pass
+    else:
+        print('shutter value before open_pv.put(0): %s' % sh.get())
+        # open shutter
+        shutter_try = 0
+        shutter_tries = 5
+        while sh.get() == 0 and shutter_try < shutter_tries:
+            sh.put(0)
+            time.sleep(2.)   
+            print('shutter value after open_pv.put(1): %s' % sh.get())
+            shutter_try += 1
+        if sh.get() == 1:
+            print('shutter failed to close after %i tries. Please check before continuing' % shutter_tries)
+            return
+
     print('Shutter closed')
