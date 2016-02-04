ff667de93fabee688860e0fd0544e322686c4417
Chia-Hao Liu <cl3077@columbia.edu>
2016-02-04 11:03:43 -0500

    add xpdRE in startup

--- xpd_profile_file/98-loadxpd.py
+++ xpd_profile_file/98-loadxpd.py
@@ -18,11 +18,13 @@ from xpdacq.config import DataPath
 from xpdacq.beamtimeSetup import *
 from xpdacq.beamtimeSetup import _make_clean_env
 from xpdacq.beamtime import XPD
-from xpdacq.xpdacq import get_light_images 
+#from xpdacq.xpdacq import *
+#from xpdacq.xpdacq import get_light_images 
 
 # These are needed in the real XPD
 WORKING_DIR = 'xpdUser'
-B_DIR = os.getcwd()
+B_DIR = os.path.expanduser('~')
+#B_DIR = os.getcwd()
 datapath = DataPath(B_DIR)
 
 print('Initializing the XPD data acquisition simulation environment') 
@@ -32,5 +34,19 @@ _make_clean_env(datapath)
 os.chdir(os.path.join(B_DIR,WORKING_DIR))
 #if there is a yml file in the normal place, then load the beamtime object
 bt = XPD.loadyamls()[0]
+
+
+# instanciate RE:
+# imports in this block can be moved up after making sure it is working
+
+import bluesky
+from bluesky.run_engine import RunEngine
+
+xpdRE = RunEnginge()
+bluesky.register_mds.register_mds(xpdRE)
+xpdRE.md['owner'] = 'xf28id1'
+xpdRE.md['beamline_id'] = 'xpd'
+xpdRE.md['group'] = 'XPD'
+
 print('OK, ready to go.  To continue, follow the steps in the xpdAcq')
 print('documentation at http://xpdacq.github.io/xpdacq')
