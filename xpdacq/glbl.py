7dc51b0664cd6e4476bea3ea575912fb3040e75f
Timothy Liu <chbrahmsliu@gmail.com>
2016-03-16 15:44:57 -0400

    revisiting glbl import
    move logic of determining simulation back to glbl.py

--- xpdacq/glbl.py
+++ xpdacq/glbl.py
@@ -19,61 +19,48 @@ OWNER = 'xf28id1'
 BEAMLINE_ID = 'xpd'
 GROUP = 'XPD'
 
-#def _areaDET(area_det_obj=None):
-#    global AREA_DET
-#    AREA_DET = area_det_obj
-
-#def _tempController(temp_controller_obj=None):
-#    global TEMP_CONTROLLER
-#    TEMP_CONTROLLER = temp_controller_obj
-
-#def _shutter(shutter_obj=None):
-#    global SHUTTER
-#    SHUTTER = shutter_obj
-
-#def _verify_write(verify_files_saved_obj=None):
-#    global VERIFY_WRITE
-#    VERIFY_WRITE = verify_files_saved_obj
-
-#def _LiveTable(livetable_obj=None):
-#    global LIVETABLE
-#    LIVETABLE = livetable_obj
-
-# analysis objects, maybe we should move them out in the future
-#def _dataBroker(databroker_obj=None):
-#    global DB
-#    DB = databroker_obj
-
-#def _getEvents(get_events_obj=None):
-#    global GET_ENV
-#    GET_ENV = get_events_obj
-
-#def _getImages(get_images_obj=None):
-#    global GET_IMG
-#    GET_IMG = get_images_obj
-
-# this block is moved to 999-load.py. Clean it if tests at XPD
-'''
-xpdRE = RunEngine()
-xpdRE.md['owner'] = 'xf28id1'
-xpdRE.md['beamline_id'] = 'xpd'
-xpdRE.md['group'] = 'XPD'
-
+# logic to assign correct objects depends on simulation or real experiment
 hostname = socket.gethostname()
 if hostname == BEAMLINE_HOST_NAME:
     # real experiment
     simulation = False
+    from bluesky.run_engine import RunEngine
     from bluesky.register_mds import register_mds
+    from bluesky import Msg
+    from bluesky.plans import Count
+    from bluesky.plans import AbsScanPlan
+    xpdRE = RunEngine()
+    xpdRE.md['owner'] = glbl.owner
+    xpdRE.md['beamline_id'] = glbl.beamline_id
+    xpdRE.md['group'] = glbl.group
     register_mds(xpdRE)
 else:
     simulation = True
     BASE_DIR = os.getcwd()
     ARCHIVE_BASE_DIR = os.path.join(BASE_DIR,'userSimulationArchive')
     xpdRE = MagicMock()
-
+    # magic mock objects
+    Msg = MagicMock()
+    Count = MagicMock()
+    AbsScanPlan = MagicMock()
+    cs700 = MagicMock()
+    db = MagicMock()
+    get_events = MagicMock()
+    get_images = MagicMock()
+    verify_files_saved = MagicMock()
+    ########################
+    shctl1 = mock_shutter()
+    LiveTable = mock_livetable
+    pe1c = MagicMock()
+    pe1c.cam = MagicMock()
+    pe1c.cam.acquire_time = MagicMock()
+    pe1c.cam.acquire_time.put = MagicMock(return_value=0.1)
+    pe1c_det.cam.acquire_time.get = MagicMock(return_value=0.1)
+    pe1c.number_of_sets = MagicMock()
+    pe1c.number_of_sets.put = MagicMock(return_value=1)
     print('==== Simulation being created in current directory:{} ===='.format(BASE_DIR))
-'''
 
+# directories
 HOME_DIR = os.path.join(BASE_DIR, HOME_DIR_NAME)
 BLCONFIG_DIR = os.path.join(BASE_DIR, BLCONFIG_DIR_NAME)
 EXPORT_DIR = os.path.join(HOME_DIR, 'Export')
@@ -115,7 +102,6 @@ class glbl():
     yaml_dir = YAML_DIR
     allfolders = ALL_FOLDERS
     archive_dir = USER_BACKUP_DIR
-    beamhost = BEAMLINE_HOST_NAME
     dk_yaml = DARK_YAML_NAME
     dk_window = DARK_WINDOW
     frame_acq_time = FRAME_ACQUIRE_TIME
@@ -139,21 +125,8 @@ class glbl():
     get_events = None
     get_images = None
     verify_files_saved = None
+   
     
-    # this block of code has been moved to 999-load.py. Clean it after testing at XPD
-    '''   
-    if hostname != BEAMLINE_HOST_NAME:
-        shutter = mock_shutter()
-        LiveTable = mock_livetable
-        #area_det = mock_areadetector()
-        area_det = MagicMock()
-        area_det.cam = MagicMock()
-        area_det.cam.acquire_time = MagicMock()
-        area_det.cam.acquire_time.put = MagicMock(return_value=1)
-        area_det.cam.acquire_time.get = MagicMock(return_value=1)
-        area_det.number_of_sets = MagicMock()
-        area_det.number_of_sets.put = MagicMock(return_value=1)
-    '''
 
 if __name__ == '__main__':
     print(glbl.dp().home)
