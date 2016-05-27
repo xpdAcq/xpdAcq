940c473a3b2cad43fe7a582141cfe81c64675a42
Timothy Liu <chbrahmsliu@gmail.com>
2016-05-27 14:06:53 -0400

    finished editing setup.py.
    full set of dependencies, bluesky, tifffile...e.t.c.
    Simulation on different platform is possible

--- setup.py
+++ setup.py
@@ -11,13 +11,14 @@ setup(
     install_requires = [
                     'numpy >= 1.11',
                     'matplotlib >= 1.5.1',
-                    'pyyaml',
+                    'PyYaml',
+                    'tifffile',
                     'boltons',
                     'bluesky',
                     'event_model'
                             ],
     dependency_links=[
-        'https://github.com/NSLS-II/event-model/zipball/master#egg=event_model-v1.0.2',
-        'https://github.com/NSLS-II/bluesky/zipball/master#egg=bluesky-v0.5.1'
+        'https://github.com/NSLS-II/event-model/zipball/v1.0.2#egg=event_model-v1.0.2',
+        "https://github.com/NSLS-II/bluesky/zipball/v0.5.2#egg=bluesky-v0.5.2"
         ]
     )
