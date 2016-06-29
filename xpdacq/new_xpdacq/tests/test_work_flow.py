d94aa242cd891490ad5f2c0f955ac506b86b0a8a
Timothy Liu <chbrahmsliu@gmail.com>
2016-06-29 16:33:47 -0400

    remove duplicated tests.
    Tests in these two files are included in other test files

--- xpdacq/new_xpdacq/tests/test_work_flow.py
+++
@@ -1,8 +0,0 @@
-from xpdacq.new_xpdacq.xpdacq import *
-from xpdacq.new_xpdacq.beamtimeSetup import _end_beamtime, _start_beamtime
-from xpdacq.new_xpdacq.beamtime import *
-bt = _start_beamtime('alva', '1234', experimenters=[], wavelength=0.1828)
-Experiment('temp_test', bt)
-ScanPlan(bt.experiments[0], ct, 10)
-Sample('test_sample', composition={})
-bt.list()
