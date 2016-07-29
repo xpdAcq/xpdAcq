e5dd0ade16398001b468effebb8f0960a80c253c
Chia-Hao Liu <cl3077@columbia.edu>
2016-07-29 12:57:55 -0400

    clean up tiff_export. Fix bug of double super()

--- xpdacq/new_xpdacq/tiff_export.py
+++ xpdacq/new_xpdacq/tiff_export.py
@@ -1,30 +1,42 @@
-from time import sleep
 import numpy as np
+from databroker import DataBroker as db
 from bluesky.callbacks.broker import LiveTiffExporter
-from .glbl import glbl
 
-class SubtractedTiffExporter(LiveTiffExporter):
+class xpdAcqSubtractedTiffExporter(LiveTiffExporter):
     "Intercept images before saving and subtract dark image"
-
     def start(self, doc):
         # The metadata refers to the scan uid of the dark scan.
         #if 'dark_frame' not in doc:
+        dark_sub = True
         if 'dark_frame' not in doc:
             if 'sc_dk_field_uid' not in doc:
                 raise ValueError("No dark_frame was recorded.")
             uid = doc['sc_dk_field_uid']
-            dark_header = glbl.db[uid]
-            self.dark_img, = glbl.get_images(glbl.db[uid], 'pe1_image')
+            dark_header = db[uid]
+            self.dark_img, = db.get_images(db[uid], 'pe1_image')
         elif 'dark_frame'in doc:
             print('WOOOP, dark frame')
-            self.dark_img = np.zeros((2048,2048))
         super().start(doc)
 
     def event(self, doc):
-        img = doc['data'][self.field]
-        subtracted_img = img - self.dark_img
-        doc['data'][self.field] = subtracted_img
-        super().event(doc)
+        if self.field not in doc['data']:
+            raise KeyError('required field = {} is not in header'.format(self.field))
+            return
 
-xpd_template = "/home/xf28id1/xpdUser/tiff_base/{start.sa_name}/{start.time}_{start.uid}_step{event.seq_num}.tif"
-xpd_exporter = SubtractedTiffExporter('pe1_image', xpd_template)
+        db.fill_event(doc)  # modifies in place
+
+        image = np.asarray(doc['data'][self.field])
+        image = np.clip(image - self.dark_img, 0, None)
+        if image.ndim == 2:
+            filename = self.template.format(start=self._start, event=doc)
+            self._save_image(image, filename)
+        if image.ndim == 3:
+            for i, plane in enumerate(image):
+                filename = self.template.format(i=i, start=self._start,
+                                                event=doc)
+                self._save_image(plane, filename)
+        return filename
+        # Don't call super() because that tries to fill_event again.
+
+xpdacq_template = "/home/xf28id1/xpdUser/{start.sa_name}_{start.time}_{start.uid}_step{event.seq_num}.tif"
+xpdacq_exporter = xpdAcqSubtractedTiffExporter('pe1_image', xpdacq_template)
