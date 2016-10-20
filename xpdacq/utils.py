4bd4b6f0776de05a78723d260b5f2c03889fbc19
Timothy Liu <chbrahmsliu@gmail.com>
2016-10-20 12:09:16 -0400

    MNT: typo

--- xpdacq/utils.py
+++ xpdacq/utils.py
@@ -21,9 +21,9 @@ def _check_obj(obj_name, error_msg=None):
     Parameter
     ---------
     obj_name : str
-        object names in string format
+        object name in string format
     error_msg : str
-        error msg when target object cant't be found in current
+        error msg when target object can't be found in current
         namespace
     """
     if error_msg is None:
@@ -147,10 +147,10 @@ def import_userScriptsEtc():
     """Import user files that have been placed in xpdUser/Import
 
     Allowed files are python user-script files (extension .py),
-    detector-image mask files (.npy) or files containing xpdAcq objects 
-    (extension .yml). Files created by running export_userScriptsEtc() are 
-    also allowed.  Unallowed files (anything not in the previous list) will 
-    be ignored. 
+    detector-image mask files (.npy) or files containing xpdAcq objects
+    (extension .yml). Files created by running export_userScriptsEtc() are
+    also allowed.  Unallowed files (anything not in the previous list) will
+    be ignored.
 
     After import, all files in the xpdUser/import directory will be deleted
     The user can run `export_userScriptsEtc` to revert them.
@@ -350,7 +350,7 @@ class ExceltoYaml:
                 elif k in self._DICT_LIKE_FIELD:
                     parsed_sa_md.update({k: self._dict_like_parser(v)})
 
-                # other fields dont need to be pased
+                # other fields don't need to be parsed
                 else:
                     parsed_sa_md.update({k: v})
 
@@ -447,12 +447,12 @@ class ExceltoYaml:
         return name_list  # [first, last]
 
     def _phase_parser(self, phase_str):
-        """ parser for filed with <chem formular>: <phase_amount>
+        """ parser for filed with <chem formula>: <phase_amount>
 
         Parameters
         ----------
         phase_str : str
-            a string contains a series of <chem formular> : <phase_amount>.
+            a string contains a series of <chem formula> : <phase_amount>.
             Each phase is separated by a comma
 
         Returns
@@ -486,7 +486,7 @@ class ExceltoYaml:
                 amount = amount.replace('%','')
             phase_dict.update({com.strip(): float(amount.strip())})
             parsed_tuple = composition_analysis(com.strip())
-            # expect: ([elment_1, element_2, ...], [sto_1, sto_2,...])
+            # expect: ([element_1, element_2, ...], [sto_1, sto_2,...])
             for i in range(len(parsed_tuple[0])):
                 composition_dict.update({parsed_tuple[0][i]:
                                              parsed_tuple[1][i]})
@@ -504,9 +504,9 @@ excel_to_yaml = ExceltoYaml()
 def import_sample_info(saf_num=None, bt=None):
     """ import sample metadata based on a spreadsheet
 
-    this function expect a prepopulated '<SAF_number>_sample.xls' file 
-    located under `xpdConfig/` directory. Corresponding Sample objects 
-    will be created after information stored being parsed. Please go to 
+    this function expects a pre-populated '<SAF_number>_sample.xls' file
+    located under `xpdUser/import` directory. Corresponding Sample objects
+    will be created after information stored being parsed. Please go to
     http://xpdacq.github.io for parser rules.
 
     Parameters
@@ -537,9 +537,9 @@ def import_sample_info(saf_num=None, bt=None):
 def _import_sample_info(saf_num=None, bt=None):
     """ core function to import sample metadata based on a spreadsheet
 
-    this function expect a prepopulated '<SAF_number>_sample.xls' file 
-    located under `xpdConfig/` directory. Corresponding Sample objects 
-    will be created after information stored being parsed. Please go to 
+    this function expects a pre-populated '<SAF_number>_sample.xlxs' file
+    located under `xpduser/import` directory. Corresponding Sample objects
+    will be created after information stored being parsed. Please go to
     http://xpdacq.github.io for parser rules.
 
     Parameters
@@ -553,9 +553,9 @@ def _import_sample_info(saf_num=None, bt=None):
     # at core function level, bt should strictly be Beamtime,
     # raise if it is not this case
     if not isinstance(bt, Beamtime):
-        raise TypeError("WARINING: illegal Beamtime object argument!.\n"
+        raise TypeError("WARNING: illegal Beamtime object argument!.\n"
                         "input object {} has type = {}\n"
-                        "Please make sure you are hadning correct object\n"
+                        "Please make sure you are handing correct object\n"
                         "or double check if you already started a beamtime"
                         .format(bt, type(bt)))
         return
@@ -565,7 +565,7 @@ def _import_sample_info(saf_num=None, bt=None):
             saf_num = bt['bt_safN']
         except KeyError:
             print("WARNING: there is no SAF number information in this "
-                  "beamtime objec.\n Do you feed in a valid beamtime "
+                  "beamtime object.\n Do you feed in a valid beamtime "
                   "object?")
             return
     else:
@@ -589,4 +589,3 @@ def _import_sample_info(saf_num=None, bt=None):
     excel_to_yaml.load(saf_num)
     excel_to_yaml.create_yaml(bt)
     return excel_to_yaml
-
