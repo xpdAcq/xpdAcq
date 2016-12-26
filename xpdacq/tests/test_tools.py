8efb4a4b187038bfaeb5dec58c15ea768b942f99
Timothy Liu <chbrahmsliu@gmail.com>
2016-12-26 11:27:25 -0500

    TST: update test with pytest

--- xpdacq/tests/test_tools.py
+++ xpdacq/tests/test_tools.py
@@ -1,19 +1,17 @@
+import pytest
+import copy
 from xpdacq.tools import clean_dict
 
-def test_clean_dict():
-    # case 1: oridinary dict, no changes
-    test = {'a': 1, 'b': 2, 'c': 3}
-    clean_dict(test, '.', ',')
-    assert test == {'a': 1, 'b': 2, 'c': 3}
-    # case 2: flat dict with target character
-    test = {'a.foo': 1, 'b.bar': 2, 'c': 3}
-    clean_dict(test, '.', ',')
-    assert test == {'a,foo': 1, 'b,bar': 2, 'c': 3}
-    # case 3: nested dict with target character
-    test = {'a.foo': {'b.bar': 2}, 'c': 3}
-    clean_dict(test, '.', ',')
-    assert test == {'a,foo': {'b,bar': 2}, 'c': 3}
-    # case 4: nested dict with target character, both key and val
-    test = {'a.foo': {'b.bar': '2.foo'}, 'c': '3.bar'}
-    clean_dict(test, '.', ',')
-    assert test == {'a,foo': {'b,bar': '2.foo'}, 'c': '3.bar'}
+@pytest.mark.parametrize("input_dict, output_dict",
+                         [({'a': 1, 'b': 2, 'c': 3},
+                           {'a': 1, 'b': 2,'c':3}),
+                          ({'a.foo': 1, 'b.bar': 2, 'c': 3},
+                           {'a,foo': 1, 'b,bar': 2, 'c': 3}),
+                          ({'a.foo': {'b.bar': 2}, 'c': 3},
+                           {'a,foo': {'b,bar': 2}, 'c': 3}),
+                          ({'a.foo': {'b.bar': '2.foo'}, 'c': '3.bar'},
+                           {'a,foo': {'b,bar': '2.foo'}, 'c': '3.bar'})
+                         ])
+def test_clean_dict(input_dict, output_dict):
+    clean_dict(input_dict, '.', ',')  # modify dict in place
+    assert input_dict == output_dict  # test equality
