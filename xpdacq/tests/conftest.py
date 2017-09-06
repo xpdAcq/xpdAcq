be49742ef1031f39fd43b49866e69da655fb35e5
Timothy Liu <chbrahmsliu@gmail.com>
2017-09-06 10:57:21 -0400

    TST: create pytest fixture in conftest

--- xpdacq/tests/conftest.py
+++ xpdacq/tests/conftest.py
@@ -27,35 +27,51 @@ from xpdacq.xpdacq_conf import (glbl_dict,
 from xpdacq.xpdacq import CustomizedRunEngine
 from xpdacq.beamtimeSetup import _start_beamtime
 from xpdacq.utils import import_sample_info, ExceltoYaml
-from xpdsim import db, cs700, xpd_pe1c, shctl1
+from xpdsim import cs700, xpd_pe1c, simple_pe1c, shctl1
 
 from pkg_resources import resource_filename as rs_fn
 
 
 @pytest.fixture(scope='module')
-def bt(home_dir):
+def db():
+    from xpdsim import db
+    yield db
+
+
+@pytest.fixture(scope='module')
+def bt(home_dir, db):
     # start a beamtime
     PI_name = 'Billinge '
     saf_num = 300000
     wavelength = 0.1812
     experimenters = [('van der Banerjee', 'S0ham', 1),
                      ('Terban ', ' Max', 2)]
-    pytest_dir = rs_fn('xpdacq', 'tests/')
     bt = _start_beamtime(PI_name, saf_num,
                          experimenters,
                          wavelength=wavelength)
+    # spreadsheet
+    pytest_dir = rs_fn('xpdacq', 'tests/')
     xlf = '300000_sample.xlsx'
     src = os.path.join(pytest_dir, xlf)
     shutil.copyfile(src, os.path.join(glbl_dict['import_dir'], xlf))
     import_sample_info(saf_num, bt)
+
     # set simulation objects
-    pe1c = xpd_pe1c # alias
+    # alias
+    #pe1c = xpd_pe1c 
+    pe1c = simple_pe1c
     configure_device(db=db, shutter=shctl1,
                      area_det=pe1c, temp_controller=cs700)
     yield bt
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope='module')
+def glbl(bt):
+    from xpdacq.glbl import glbl
+    yield glbl
+
+
+@pytest.fixture(scope='module')
 def fresh_xrun(bt):
     # create xrun
     xrun = CustomizedRunEngine(None)
@@ -63,10 +79,16 @@ def fresh_xrun(bt):
     xrun.md['group'] = glbl_dict['group']
     xrun.md['facility'] = glbl_dict['facility']
     xrun.ignore_callback_exceptions = False
-    xrun.beamtime = bt
     # link mds
     xrun.subscribe(xpd_configuration['db'].insert, 'all')
     yield xrun
+
+
+@pytest.fixture(scope='function')
+def exp_hash_uid(bt, fresh_xrun, glbl):
+    fresh_xrun.beamtime = bt
+    exp_hash_uid = glbl['exp_hash_uid']
+    yield exp_hash_uid
 
 
 @pytest.fixture(scope='module')
