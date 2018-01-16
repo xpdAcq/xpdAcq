ca463d507bd46fe11cc19f42467757add2f559aa
Timothy Liu <chbrahmsliu@gmail.com>
2018-01-15 21:11:31 -0500

    CLN: exclude outdated ipython prompt file

--- xpdacq/ip_prompts.py
+++
@@ -1,8 +0,0 @@
-from IPython.terminal.prompts import Prompts, Token
-
-class CollectionPrompts(Prompts):
-    def in_prompt_token(self, cli=None):
-        return [(Token, '(collection-)'),
-                (Token.Prompt, 'In['),
-                (Token.PromptNum, str(self.shell.execution_count)),
-                (Token.Prompt, ']: ')]
