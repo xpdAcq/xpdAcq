575a6d074fd5d8568697645814cc48696a9b1453
Timothy Liu <chbrahmsliu@gmail.com>
2018-01-16 13:22:17 -0500

    ENH: print filter positions for evry xrun

--- xpdacq/xpdacq.py
+++ xpdacq/xpdacq.py
@@ -232,6 +232,9 @@ def _inject_filter_positions(msg):
     if msg.command == 'open_run':
         filter_bank = xpd_configuration['filter_bank']
         filters = ['flt1', 'flt2', 'flt3', 'flt4']
+        print("INFO: Current filter status")       
+        for el in filters:
+            print("INFO: {} : {}".format(el, getattr(filter_bank, el).value))
         msg.kwargs['filter_positions'] = {fltr: getattr(filter_bank, fltr).value for fltr in filters}
     return msg
 
