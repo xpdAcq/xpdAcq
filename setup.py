20f4e8d5530765066c77310d43862bfe4b87e306
christopher <cjwright4242@gmail.com>
2018-04-22 18:26:44 -0400

    bumped version to 0.8.0rc2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='xpdacq',
-    version='0.8.0rc',
+    version='0.8.0rc2',
     packages=find_packages(),
     description='acquisition module',
     zip_safe=False,
