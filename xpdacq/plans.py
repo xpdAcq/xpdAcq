a082f3c9593e4fadb5e25c92a93d5ce748e210ac
christopher <cjwright4242@gmail.com>
2018-08-07 17:02:01 -0400

    ENH: use desired exposure as exposure target

--- xpdacq/plans.py
+++ xpdacq/plans.py
@@ -5,33 +5,32 @@ import bluesky.plans as bp
 import bluesky.plan_stubs as bps
 from xpdacq.xpdacq_conf import xpd_configuration
 
-det = xpd_configuration['area_det']
-shutter = xpd_configuration['shutter']
-fb = xpd_configuration['filter_bank']
 sh_open = xpd_configuration['open']
 sh_close = xpd_configuration['close']
 fb_attenuation = np.asarray([2, 4, 8, 16])
 
 
 # Acq
-def light_dark():
+def dark_light():
     """Plan to take a light then a dark and subtract the two for use in plans
     """
     # take dark
-    yield from bps.abs_set(shutter, sh_close, wait=True)
-    dark = yield from bps.trigger_and_read(det, name='dark')
+    yield from bps.abs_set(xpd_configuration['shutter'], sh_close, wait=True)
+    dark = yield from bps.trigger_and_read(xpd_configuration['area_det'],
+                                           name='dark')
     # take data
-    yield from bps.abs_set(shutter, sh_open, wait=True)
-    light = yield from bps.trigger_and_read(det)
-    yield from bps.abs_set(shutter, sh_close, wait=True)
+    yield from bps.abs_set(xpd_configuration['shutter'], sh_open, wait=True)
+    light = yield from bps.trigger_and_read(xpd_configuration['area_det'])
+    yield from bps.abs_set(xpd_configuration['shutter'], sh_close, wait=True)
     data = light.astype(np.float32) - dark.astype(np.float32)
     return data
 
 
 def tune_filters(upper_threshold=8000,
                  percentile=100 - .01,
-                 lower_theshold=500):
-    """Tune the filters to minimize the exposure time while not burning the
+                 lower_threshold=500,
+                 desired_exposure=.1):
+    """Tune the filters to the desired exposure time while not burning the
     detector.
 
     Parameters
@@ -41,14 +40,18 @@ def tune_filters(upper_threshold=8000,
     percentile : float
         Percentile to use to measure the max intensity (use percentile because
         of bad pixels)
-    lower_theshold : float
+    lower_threshold : float
         Threshold below which data is unreliable for measuring sample
         scattering power
+    desired_exposure : float
+        The desired exposure. The exposure must be within the capabilities of
+        the detector. This will also be used to sample the sample's scattering
+        power.
 
     Returns
     -------
     plan : generator
-        The plan:
+        The plan
 
     Notes
     -----
@@ -56,43 +59,54 @@ def tune_filters(upper_threshold=8000,
 
     """
     # List of filter configurations in order of filter power
-    # Need to set this since it could be non-unique
+    # TODO: Need to set this since it could be non-unique
     filter_configurations = sorted(
         [np.asarray(x) for x in product([0, 1], repeat=4)],
         key=lambda x: np.prod((x * fb_attenuation)[np.nonzero(x)]),
         reverse=True
     )
-    attenuations = np.asarray(
+    attenuations = 1/np.asarray(
         [np.prod((x * fb_attenuation)[np.nonzero(x)]) for x in
          filter_configurations])
 
-    @bpp.stage_decorator([det])
+    @bpp.stage_decorator([xpd_configuration['area_det']])
     @bpp.run_decorator()
     def inner():
-        yield from bps.abs_set(det.cam.acquire_time, .1)
+        yield from bps.abs_set(xpd_configuration['area_det'].cam.acquire_time,
+                               desired_exposure)
         for fc in filter_configurations:
             att = 1 / np.prod((fc * fb_attenuation)[np.nonzero(fc)])
             # set the filter banks
-            for f, io in zip(fb, fc):
-                yield from bps.abs_set(f, io, wait=True)
-            data = yield from light_dark()
+            yield from (bps.abs_set(f, io, wait=True) for f, io in
+                        zip(xpd_configuration['filter_bank'], fc))
+            data = yield from dark_light()
 
             # calculate the value of the top .01 percentile pixel
             v = np.percentile(data, percentile)
 
             # The value is too low to evaluate, bump to next configuration
-            if v < lower_theshold:
+            if v < lower_threshold:
                 continue
+            else:
+                break
         # compute the sample scattering power
-        sp = v / att
+        sp = v / (att * desired_exposure)
         # compute filter config index to use
-        fci = np.where((sp / attenuations) < upper_threshold)[0][-1]
+        fci = np.where((sp * attenuations * desired_exposure
+                        ) < upper_threshold)[0][-1]
         # set the bank
-        for f, io in zip(fb, filter_configurations[fci]):
-            yield from bps.abs_set(f, io, wait=True)
+        yield from (bps.abs_set(f, io, wait=True) for f, io in
+                    zip(xpd_configuration['filter_bank'],
+                        filter_configurations[fci]))
 
         # compute the exposure
-        exposure = min(5, upper_threshold / (sp / attenuations[fci]))
-        yield from bps.abs_set(det.cam.acquire_time, exposure)
+        computed_exposure = upper_threshold / (sp * attenuations[fci])
+        print('Extrapolated exposure: {}'.format(computed_exposure))
+        exposure = min(5, computed_exposure)
+        print('Using exposure: {}'.format(computed_exposure))
+        expected_counts = sp * attenuations[fci] * exposure
+        print('Expected counts: {}'.format(expected_counts))
+        yield from bps.abs_set(xpd_configuration['area_det'].cam.acquire_time,
+                               exposure)
 
     return (yield from inner())
