67c31ef2b214d14dbbfd79210b25118385b1beb4
Christopher J. Wright <cjwright4242@gmail.com>
2019-04-16 16:26:05 -0400

    bumped version to 0.10.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='xpdacq',
-    version='0.9.1',
+    version='0.10.0',
     packages=find_packages(),
     description='acquisition module',
     zip_safe=False,
