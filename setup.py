d4b368fb27f61bffa93d71acfeb93e71f88c9bc8
Timothy Liu <chbrahmsliu@gmail.com>
2019-09-11 17:26:27 -0400

    bumped version to 0.10.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='xpdacq',
-    version='0.10.1',
+    version='0.10.3',
     packages=find_packages(),
     description='acquisition module',
     zip_safe=False,
