c6baec8827a07a69d533d57ac79ba9755405940c
Thomas A Caswell <tcaswell@bnl.gov>
2020-08-30 13:22:03 -0400

    BLD: add dependencies to setup.py
    I am not sure that these are correct.

--- setup.py
+++ setup.py
@@ -2,13 +2,27 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='xpdacq',
-    version='0.10.4',
+    name="xpdacq",
+    version="0.10.4",
     packages=find_packages(),
-    description='acquisition module',
+    description="acquisition module",
     zip_safe=False,
-    package_data={'xpdacq': ['data/*.D', 'tests/*.D',
-                             'tests/*.xls*', 'tests/*.yml',
-                             'examples/*.yaml']},
-    url='http:/github.com/xpdAcq/xpdacq'
+    package_data={
+        "xpdacq": [
+            "data/*.D",
+            "tests/*.D",
+            "tests/*.xls*",
+            "tests/*.yml",
+            "examples/*.yaml",
+        ]
+    },
+    url="http:/github.com/xpdAcq/xpdacq",
+    install_requires=[
+        "numpy>=1.11",
+        "matplotlib>=1.5.1",
+        "pyyaml",
+        "boltons",
+        "bluesky>=v0.5.1",
+        "event_model",
+    ],
 )
