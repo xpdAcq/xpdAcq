34bdb6e59e2fcd0b7bae2c72ca33a43627293732
st3107 <st3107@columbia.edu>
2020-11-23 15:59:09 -0500

    ENH: edit 999-load2

--- xpdacq/999-load2.py
+++ xpdacq/999-load2.py
@@ -1,20 +1,17 @@
 import os
 from pathlib import Path
-from xpdan.data_reduction import save_last_tiff, save_tiff, integrate_and_save, integrate_and_save_last
+
+from xpdsim import xpd_pe1c, shctl1, cs700, db, ring_current, fb
 
 from xpdacq.beamtime import ScanPlan, Sample, ct, Tramp, Tlist, tseries
 from xpdacq.beamtimeSetup import _start_beamtime, _end_beamtime
 from xpdacq.calib import run_calibration
 from xpdacq.ipysetup import ipysetup
 from xpdacq.utils import import_userScriptsEtc, import_sample_info
-from xpdacq.xpdacq_conf import glbl_dict
 from xpdacq.xpdacq_conf import xpd_configuration
 
-if glbl_dict["is_simulation"]:
-    from xpdsim import xpd_pe1c, shctl1, cs700, db, ring_current, fb
-
-    pe1c = xpd_pe1c
-    del xpd_pe1c
+pe1c = xpd_pe1c
+del xpd_pe1c
 
 _start_beamtime = _start_beamtime
 _end_beamtime = _end_beamtime
@@ -27,10 +24,6 @@ Tramp = Tramp
 Tlist = Tlist
 tseries = tseries
 run_calibration = run_calibration
-save_last_tiff = save_last_tiff
-save_tiff = save_tiff
-integrate_and_save = integrate_and_save
-integrate_and_save_last = integrate_and_save_last
 xpd_configuration = xpd_configuration
 print("INFO: Initializing the XPD data acquisition environment ...")
 glbl, bt, xrun = ipysetup(
@@ -42,9 +35,12 @@ glbl, bt, xrun = ipysetup(
     db=db
 )
 print("INFO: Initialized glbl, bt, xrun.")
-home = glbl["home"] if Path(glbl["home"]).is_dir() else glbl["base"]
-os.chdir(home)
-print("INFO: Changed home to {}".format(home))
+if Path(glbl["home"]).is_dir():
+    os.chdir(glbl["home"])
+    print("INFO: Changed home to {}".format(glbl["home"]))
+elif Path(glbl["base"]).is_dir():
+    os.chdir(glbl["base"])
+    print("INFO: Changed home to {}".format(glbl["base"]))
 print(
     "OK, ready to go.  To continue, follow the steps in the xpdAcq"
     "documentation at http://xpdacq.github.io/xpdacq"
