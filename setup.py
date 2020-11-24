e21b50b1f5ff857785d63313d83aa89ac8d95998
st3107 <st3107@columbia.edu>
2020-11-24 18:27:44 -0500

    BLD: debug setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ with open(path.join(here, 'requirements/run.txt')) as requirements_file:
     # Parse requirements.txt, ignoring any commented-out lines.
     requirements = [
         line
-        for line in requirements_file.read().splitlines()[1:]
+        for line in requirements_file.read().splitlines()
         if not line.startswith('#')
     ]
 
