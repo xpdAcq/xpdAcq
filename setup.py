f2c48e24346f822efc77e582bab2f997c1f5d962
st3107 <st3107@columbia.edu>
2020-11-25 12:40:20 -0500

    BLD: delete the requirements

--- setup.py
+++ setup.py
@@ -22,14 +22,6 @@ if sys.version_info < min_version:
 
 here = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, 'requirements/run.txt')) as requirements_file:
-    # Parse requirements.txt, ignoring any commented-out lines.
-    requirements = [
-        line
-        for line in requirements_file.read().splitlines()
-        if not line.startswith('#')
-    ]
-
 with open(path.join(here, 'README.md'), encoding='utf-8') as readme_file:
     readme = readme_file.read()
 
