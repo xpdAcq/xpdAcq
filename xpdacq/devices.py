2579f82c78635f2403d8fca70938aca132197dff
st3107 <st3107@columbia.edu>
2021-04-23 14:33:11 -0400

    MNT: change config to Kind.config

--- xpdacq/devices.py
+++ xpdacq/devices.py
@@ -1,17 +1,18 @@
 """Wrappers for the ophyd devices."""
 from ophyd import Device, Signal
+from ophyd import Kind
 from ophyd.device import Component as Cpt
 
 
 class CalibrationData(Device):
     """A device to hold pyFAI calibration data."""
-    dist = Cpt(Signal, value=1., kind='config')
-    poni1 = Cpt(Signal, value=0., kind='config')
-    poni2 = Cpt(Signal, value=0., kind='config')
-    rot1 = Cpt(Signal, value=0., kind='config')
-    rot2 = Cpt(Signal, value=0., kind='config')
-    rot3 = Cpt(Signal, value=0., kind='config')
-    pixel1 = Cpt(Signal, value=0., kind='config')
-    pixel2 = Cpt(Signal, value=0., kind='config')
-    detector = Cpt(Signal, value="", kind='config')
-    wavelength = Cpt(Signal, value=0., kind='config')
+    dist = Cpt(Signal, value=1., kind=Kind.config)
+    poni1 = Cpt(Signal, value=0., kind=Kind.config)
+    poni2 = Cpt(Signal, value=0., kind=Kind.config)
+    rot1 = Cpt(Signal, value=0., kind=Kind.config)
+    rot2 = Cpt(Signal, value=0., kind=Kind.config)
+    rot3 = Cpt(Signal, value=0., kind=Kind.config)
+    pixel1 = Cpt(Signal, value=0., kind=Kind.config)
+    pixel2 = Cpt(Signal, value=0., kind=Kind.config)
+    detector = Cpt(Signal, value="", kind=Kind.config)
+    wavelength = Cpt(Signal, value=0., kind=Kind.config)
