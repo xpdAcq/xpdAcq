fde83407ce48d45f73c0460e7e4dfa510e3802a9
st3107 <st3107@columbia.edu>
2022-03-30 16:45:50 -0400

    TST: test the ct, Tramp and Tlist

---
+++ xpdacq/tests/test_customized_plans.py
@@ -0,0 +1,56 @@
+# Add tests for the ct, Tlist and Tramp
+import xpdacq.beamtime as xbt
+from xpdacq.beamtime import xpd_configuration
+from xpdacq.simulators import WorkSpace
+
+
+def test_configure_area_det():
+    ws = WorkSpace()
+    ws.RE(xbt.configure_area_det(ws.det, 5., 0.1))
+    assert ws.det.cam.acquire_time.get() == 0.1
+    assert ws.det.images_per_set.get() == 50
+
+
+def test_ct():
+    ws = WorkSpace()
+    RE = ws.RE
+    det = ws.det
+    db = ws.db
+    del ws
+    xpd_configuration["area_det"] = det
+    RE(xbt.ct([det], 5.))
+    key = "{}_image".format(det.name)
+    data = list(db[-1].data(key))
+    assert len(data) == 1
+
+
+def test_Tlist():
+    ws = WorkSpace()
+    RE = ws.RE
+    det = ws.det
+    db = ws.db
+    eurotherm = ws.eurotherm
+    del ws
+    xpd_configuration["area_det"] = det
+    xpd_configuration["temp_controller"] = eurotherm
+    setpoints = [100., 200., 300.]
+    RE(xbt.Tlist([det], 5., setpoints))
+    key = eurotherm.name
+    measured = list(db[-1].data(key))
+    assert measured == setpoints
+
+
+def test_Tramp():
+    ws = WorkSpace()
+    RE = ws.RE
+    det = ws.det
+    db = ws.db
+    eurotherm = ws.eurotherm
+    del ws
+    xpd_configuration["area_det"] = det
+    xpd_configuration["temp_controller"] = eurotherm
+    setpoints = [100., 200., 300.]
+    RE(xbt.Tramp([det], 5., 100., 300., 100.))
+    key = eurotherm.name
+    measured = list(db[-1].data(key))
+    assert measured == setpoints
