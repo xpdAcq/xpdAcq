d2499eb8bbfb46ade13154e75022816ed12ca402
st3107 <st3107@columbia.edu>
2022-04-04 16:36:37 -0400

    TST: test RunCalibration

---
+++ xpdacq/tests/test_calib2.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+import pytest
+import shutil
+from pkg_resources import resource_filename
+from xpdacq.calib2 import RunCalibration
+from xpdacq.preprocessors import CalibPreprocessor
+from xpdacq.xpdacq import CustomizedRunEngine
+from xpdacq.simulators import WorkSpace
+
+_PONI_FILE = Path(resource_filename("xpdacq", "tests/Ni_poni_file.poni"))
+
+
+@pytest.fixture
+def poni_file(tmp_path):
+    fp = tmp_path.joinpath(_PONI_FILE.name)
+    shutil.copy(str(_PONI_FILE), str(fp))
+    return fp
+
+
+def test_without_running_pyFAI(fresh_xrun, poni_file):
+    xrun = fresh_xrun
+    del fresh_xrun
+    ws = WorkSpace()
+    cpp = CalibPreprocessor(ws.det)
+    xrun.calib_preprocessors.append(cpp)
+    glbl = {
+        "config_base": str(poni_file.parent),
+        "calib_config_name": poni_file.name,
+        "frame_acq_time": 0.1
+    }
+    run_calibration = RunCalibration(xrun, glbl)
+    run_calibration(wait_for_cal=False)
+    assert len(cpp._cache) == 1
