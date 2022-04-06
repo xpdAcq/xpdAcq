aaa46bda294e30ab2088ac113adba7b668a358aa
st3107 <st3107@columbia.edu>
2022-04-06 11:44:22 -0400

    TST: test __repr__

--- xpdacq/tests/preprocessors/test_shutterpreprocessor.py
+++ xpdacq/tests/preprocessors/test_shutterpreprocessor.py
@@ -26,3 +26,13 @@ def test_call():
     ]
     real = [(msg.command, msg.obj) for msg in msgs[:len(expected)]]
     assert real == expected
+
+
+def test_repr():
+    shutter = get_shutter(name="shutter")
+    detector = get_detector(name="detector")
+    spp = ShutterPreprocessor(
+        detector=detector,
+        shutter_config=ShutterConfig(shutter, "open", "closed")
+    )
+    assert spp.__repr__() == "<ShutterPreprocessor for detector>"
