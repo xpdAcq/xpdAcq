21bb2d137bc3cab0b5af61352bbea760974a4ef1
st3107 <st3107@columbia.edu>
2022-04-07 13:17:41 -0400

    DOC: add docstring for ShutterConfig

--- xpdacq/preprocessors/shutterconfig.py
+++ xpdacq/preprocessors/shutterconfig.py
@@ -6,6 +6,19 @@ from ophyd import Signal
 
 @dataclass
 class ShutterConfig:
+    """The Configuration of the shutter control.
+
+    Attributes
+    ----------
+    shutter : Signal
+        The ophyd object of shutter.
+    open_state: Any
+        The scalar value of the shutter position when it is open.
+    close_state: Any
+        The scalar value of the shutter position when it is closed.
+    delay : float
+        The delay time after shutter open or close in seconds, default 0.0.
+    """
 
     shutter: Signal
     open_state: T.Any
@@ -14,6 +27,7 @@ class ShutterConfig:
 
     @classmethod
     def from_xpdacq(cls):
+        """Use the setting from the global configuration of xpdAcq."""
         from xpdacq.xpdacq_conf import xpd_configuration
         from xpdconf.conf import XPD_SHUTTER_CONF
         return cls(
