39d97739d530dab4ea6964392a9b789cd7bc85a3
st3107 <st3107@columbia.edu>
2022-04-07 17:06:25 -0400

    MNT: format code

--- xpdacq/preprocessors/shutterpreprocessor.py
+++ xpdacq/preprocessors/shutterpreprocessor.py
@@ -11,9 +11,13 @@ Plan = T.Generator[Msg, T.Any, T.Any]
 
 
 class ShutterPreprocessor:
-    """The preprocessor to mutate the plan so that the shutter will be open before every non dark trigger of the detector and closed after the trigger finishes.
+    """The preprocessor to mutate the plan so that the shutter will be
+    open before every non dark trigger of the detector and closed after
+    the trigger finishes.
 
-    This preprocessor must be used after the DarkPreprocessor. It won't open the shutter for the trigger to take the dark frame. It will open the shutter only for those triggers in non-dark groups.
+    This preprocessor must be used after the DarkPreprocessor. It won't
+    open the shutter for the trigger to take the dark frame. It will
+    open the shutter only for those triggers in non-dark groups.
 
     Parameters
     ----------
@@ -26,12 +30,12 @@ class ShutterPreprocessor:
     """
 
     def __init__(
-        self, 
+        self,
         *,
         detector: Device,
         dark_group_prefix: str = 'bluesky-darkframes-trigger',
         shutter_config: T.Optional[ShutterConfig] = None,
-        ) -> None:
+    ) -> None:
         if shutter_config is None:
             shutter_config = ShutterConfig.from_xpdacq()
         self._detector = detector
@@ -62,7 +66,6 @@ class ShutterPreprocessor:
                 yield from bps.mv(shutter, close_state)
             return
 
-
         def _mutate(msg: Msg):
             # open the shutter before a non dark trigger
             group = msg.kwargs.get("group")
@@ -88,6 +91,6 @@ class ShutterPreprocessor:
         """
         self._disabled = False
         return
-    
+
     def __repr__(self) -> str:
         return "<{} for {}>".format(self.__class__.__name__, self._detector.name)
