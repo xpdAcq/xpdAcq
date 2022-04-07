f55ebdc7d1ab644d606e931440a5caa8d448a6ff
st3107 <st3107@columbia.edu>
2022-04-07 15:47:17 -0400

    TST: update test_ipysetup

--- xpdacq/tests/test_ipysetup.py
+++ xpdacq/tests/test_ipysetup.py
@@ -4,20 +4,19 @@ from xpdsim import xpd_pe1c, shctl1, cs700, ring_current, fb
 from xpdacq.ipysetup import UserInterface
 
 
-def test_ipysetup(capfd, beamline_config_file):
-    with capfd.disabled():
-        db = Broker.named("temp")
-        ui = UserInterface(
-            area_dets=[xpd_pe1c],
-            det_zs=[None],
-            shutter=shctl1,
-            temp_controller=cs700,
-            filter_bank=fb,
-            ring_current=ring_current,
-            db=db,
-            blconfig_yaml=beamline_config_file,
-            test=True
-        )
+def test_ipysetup(beamline_config_file):
+    db = Broker.named("temp")
+    ui = UserInterface(
+        area_dets=[xpd_pe1c],
+        det_zs=[None],
+        shutter=shctl1,
+        temp_controller=cs700,
+        filter_bank=fb,
+        ring_current=ring_current,
+        db=db,
+        blconfig_yaml=beamline_config_file,
+        test=True
+    )
     assert ui.glbl
     assert not ui.bt
     assert ui.xrun
