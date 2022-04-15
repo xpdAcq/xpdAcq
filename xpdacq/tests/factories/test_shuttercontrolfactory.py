b6ba0c1485a8773dae15745359dfb4d56c320843
st3107 <st3107@columbia.edu>
2022-04-15 10:06:58 -0400

    TST: debug the imports

--- xpdacq/tests/factories/test_shuttercontrolfactory.py
+++ xpdacq/tests/factories/test_shuttercontrolfactory.py
@@ -1,15 +1,15 @@
 from bluesky.simulators import summarize_plan
 from xpdacq.factories.shuttercontrolfactory import ShutterControlFactory
-from xpdacq.simulators import get_close_shutter, get_open_shutter, get_shutter, get_detector
+from xpdacq.simulators import get_close_shutter, get_open_shutter, Shutter, PerkinElmerDetector
 
 
 def test_get_take_reading(capsys):
-    shutter = get_shutter()
+    shutter = Shutter(name="shutter")
     scf = ShutterControlFactory(
         open_shutter=get_open_shutter(shutter),
         close_shutter=get_close_shutter(shutter)
     )
-    detector = get_detector()
+    detector = PerkinElmerDetector(name="detector")
     take_reading = scf.get_take_reading()
     summarize_plan(take_reading([detector]))
     captured = capsys.readouterr()
